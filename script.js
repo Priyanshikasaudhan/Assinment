8c4ebcbe1b42e661d184a7c1a016bd8cf612052d
PRIYANSHI KASAUDHAN <147182966+Priyanshikasaudhan@users.noreply.github.com>
2024-07-15 19:55:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -227,17 +227,17 @@ document.querySelectorAll('.about-photos').forEach(function (el) {
 
 const contentArray = [
     {
-        imagePath: 'Assets/images/japan1.jpg',
+        imagePath: 'japan1.jpg',
         title: 'Genderless Kei - Japan\'s Hot',
         description: 'Set to launch on the manufacturer\'s new A330neo aircraft in 2017, it\'s offering lots of'
     },
     {
-        imagePath: 'Assets/images/image.png',
+        imagePath: 'image.png',
         title: 'Better Strategy & Quality',
         description: 'Set to launch on the manufacturer\'s new A330neo aircraft in 2017, it\'s offering lots of'
     },
     {
-        imagePath: 'Assets/images/japan3.jpg',
+        imagePath: 'japan1.jpg',
         title: 'Genderless Kei - Japan\'s Hot',
         description: 'Set to launch on the manufacturer\'s new A330neo aircraft in 2017, it\'s offering lots of'
     }
